0cd807bfb732a89561962ee2b7f17b798848cb4f
Samar_Cherif <samar.cherif10@gmail.com>
2026-02-17 14:15:21 +0100

    updated i18n.ts

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,16 +1,13 @@
 // Importe les outils de next-intl
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
 // 1. DÉFINIR LES LANGUES DISPONIBLES
 export const locales = ["fr", "ar", "en", "es", "de", "it"] as const;
-export type Locale = (typeof locales)[number]; 
+export type Locale = (typeof locales)[number];
 export const defaultLocale: Locale = "fr";
-
 // 2. CONFIGURATION next-intl
 export default getRequestConfig(async ({ locale }) => {
-  if (!locale ||!locales.includes(locale as Locale)) notFound();
-
+  if (!locale || !locales.includes(locale as Locale)) notFound();
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default,
